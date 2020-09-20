ff5c4fec5009ad8fce8451ff226d27dd95930164
Sidney Ng <sng@chewy.com>
2020-09-20 17:11:04 -0400

    Add .. as the go back link

--- UI/src/Components/Directory.js
+++ UI/src/Components/Directory.js
@@ -29,14 +29,28 @@ const Directory = (props) => {
 		setCurrentDir(currentDir.path.concat('/', dirName));
 	};
 
-	const parentDirLink = (
-		<Box key={0} my={1}>
-			<FileLink name={'../'} handleClick={event => {
-				event.preventDefault();
-				console.log('click')
-			}} />
-		</Box>
-	);
+	const parentDirLink = () => {
+		// Create the link; this could be done through a selector but that's O(n^2) and this is constant time
+		const pathDirs = currentDir.path.split(/[./]/);
+		const destinationPath = ['.'];
+		for (let i = 0; i < pathDirs.length-1; i++) {
+			// ignore empty string because '.' is at the beginning of the string
+			if (pathDirs[i] === '') {
+				continue;
+			}
+			destinationPath.push(pathDirs[i]);
+		}
+
+
+		return (
+			<Box key={0} my={1}>
+				<FileLink name={'../'} handleClick={event => {
+					event.preventDefault();
+					setCurrentDir(destinationPath.join('/'));
+				}} />
+			</Box>
+		);
+	};
 
 	let directories = currentDir.directories.map((file, index) =>
 		<Box key={index + 1} my={1}>
@@ -49,7 +63,7 @@ const Directory = (props) => {
 
 
 	if (rootDir !== currentDir.path) {
-		directories = [parentDirLink].concat(directories);
+		directories = [parentDirLink()].concat(directories);
 	}
 
 	const files = currentDir.files.map((file, index) =>
