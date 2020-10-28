189b524a9888cf9e57ee8dca81bc34c63edd8e77
Sidney Ng <sng@chewy.com>
2020-10-27 21:06:15 -0400

    reformatted backend

--- backend/app.py
+++ backend/app.py
@@ -5,16 +5,19 @@ from flask import Flask
 app = Flask(__name__)
 rootPath = "./Files"
 
+
 @app.route('/')
 def root():
-	return
+    return
+
 
 @app.route('/tree')
-def getFileTree():
-	directories = getDirectories()
-	return json.dumps({"root": rootPath, "directories": directories})
+def get_file_tree():
+    directories = get_directories()
+    return json.dumps({"root": rootPath, "directories": directories})
 
-# [{"directories": ["project1", "project3", "project2"], "files": [], "path": "./Files"}, 
+
+# [{"directories": ["project1", "project3", "project2"], "files": [], "path": "./Files"},
 # {"directories": ["notes", "examples"], "files": ["README.md"], "path": "./Files/project1"}, 
 # {"directories": [], "files": ["notes1.txt"], "path": "./Files/project1/notes"}, 
 # {"directories": ["xml", "json"], "files": [], "path": "./Files/project1/examples"}, 
@@ -24,15 +27,16 @@ def getFileTree():
 # {"directories": ["goes"], "files": [], "path": "./Files/project2/this"}, {"directories": ["deep"], "files": [], "path": "./Files/project2/this/goes"}, 
 # {"directories": [], "files": ["treasure.txt"], "path": "./Files/project2/this/goes/deep"}]
 
-def getDirectories():
-	fileTree = []
-	for (root, dirs, files) in os.walk(rootPath):
-		# If you used an object, the lists wouldn't serialize to json. just using a pure dictionary will serialize all fields
-		directory = {"directories": [], "files": [], "path": root}
-		for file in files:
-			directory["files"].append(file)
-		for dirname in dirs:
-			directory["directories"].append(dirname)
-		fileTree.append(directory)
+def get_directories():
+    file_tree = []
+    for (root, dirs, files) in os.walk(rootPath):
+        # If you used an object, the lists wouldn't serialize to json.
+        # just using a pure dictionary will serialize all fields
+        directory = {"directories": [], "files": [], "path": root}
+        for file in files:
+            directory["files"].append(file)
+        for dirname in dirs:
+            directory["directories"].append(dirname)
+        file_tree.append(directory)
 
-	return fileTree
+    return file_tree
